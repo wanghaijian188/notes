c9678db03332246cd2cdb0b6576ce2a6b1d81f43
whj <1469202826@qq.com>
2021-10-26 10:48:54 +0800

    12

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 echo 'notes.wanghaijian.cn' >CNAME
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:WangHaijian188/acolasia.git master:gh-pages
+git push -f git@github.com:wanghaijian188/notes.git master:gh-pages
 
 cd -
